62536e89f97459e61f69036aeb0a907b656159e1
Ben Karel <bkarel@aarno-labs.com>
2025-07-07 16:24:25 -0400

    NFC: Extract recently-added helper code to `speculative_rewriters.py`

---
+++ cli/speculative_rewriters.py
@@ -0,0 +1,93 @@
+from pathlib import Path
+from typing import Callable, Sequence
+from dataclasses import dataclass
+
+from dataclasses_json import dataclass_json
+
+
+# XREF:CONDENSED_SPAN_GRAPH_REPRESENTATION
+@dataclass_json
+@dataclass
+class ExplicitSpan:
+    fileid: int
+    lo: int
+    hi: int
+
+
+@dataclass_json
+@dataclass
+class CondensedSpanGraph:
+    files: list[Path]
+    elts: list[ExplicitSpan]  # full spans for functions
+    nodes: list[list[int]]  # indices into `elts`
+    edges: list[tuple[int, int]]  # indices into `nodes`
+
+
+class SpeculativeFileRewriter:
+    def __init__(self, p: Path):
+        self.p = p
+        self.original_content = p.read_text("utf-8")
+        self.cached_content = self.original_content
+
+    def update_content_via(self, fn: Callable[[str], str]):
+        self.cached_content = fn(self.cached_content)
+
+    def write(self) -> bool:
+        """Returns true if any changes were made."""
+        if self.cached_content != self.original_content:
+            self.p.write_text(self.cached_content, encoding="utf-8")
+            return True
+        return False
+
+    def restore(self):
+        self.p.write_text(self.original_content, encoding="utf-8")
+
+
+class SpeculativeSpansEraser:
+    def __init__(
+        self, spans: Sequence[ExplicitSpan | None], span_to_path: Callable[[ExplicitSpan], Path]
+    ):
+        self.spans = spans
+        self.rewriters: dict[Path, SpeculativeFileRewriter] = {}
+        self.spans_by_path: dict[Path, list[ExplicitSpan]] = {}
+
+        for span in spans:
+            if span is not None:
+                file_path = span_to_path(span)
+                if file_path not in self.rewriters:
+                    self.spans_by_path.setdefault(file_path, []).append(span)
+                    self.rewriters[file_path] = SpeculativeFileRewriter(file_path)
+
+    def erase_spans(self):
+        def erase_span(span: ExplicitSpan, content: str) -> str:
+            """Erase the span from the content."""
+            assert span.hi > span.lo, "Span must be non-empty"
+            assert span.hi <= len(content), "Span end must be within content bounds"
+            return content[: span.lo] + (" " * (span.hi - span.lo)) + content[span.hi :]
+
+        for rewriter in self.rewriters.values():
+            for span in self.spans_by_path[rewriter.p]:
+                rewriter.update_content_via(lambda content, span=span: erase_span(span, content))
+            rewriter.write()
+
+    def restore(self):
+        for rewriter in self.rewriters.values():
+            rewriter.restore()
+
+
+class SpeculativeFilesEraser:
+    def __init__(self, files: Sequence[Path]):
+        self.files = files
+        self.rewriters: dict[Path, SpeculativeFileRewriter] = {
+            file: SpeculativeFileRewriter(file) for file in files
+        }
+
+    def add_file(self, file: Path) -> SpeculativeFileRewriter:
+        if file not in self.rewriters:
+            self.rewriters[file] = SpeculativeFileRewriter(file)
+
+        return self.rewriters[file]
+
+    def restore(self):
+        for rewriter in self.rewriters.values():
+            rewriter.restore()
--- cli/translation.py
+++ cli/translation.py
@@ -6,13 +6,16 @@ import re
 import os
 import json
 import graphlib
-from dataclasses import dataclass
-from typing import Callable, Sequence
+from typing import Sequence
 import time
 
-from dataclasses_json import dataclass_json
-
 import hermetic
+from speculative_rewriters import (
+    CondensedSpanGraph,
+    ExplicitSpan,
+    SpeculativeFileRewriter,
+    SpeculativeSpansEraser,
+)
 
 
 def do_translate(
@@ -156,94 +159,6 @@ def run_improve_multitool(root: Path, tool: str, args: list[str], dir: Path):
         },
         check=True,
     )
-
-
-# XREF:CONDENSED_SPAN_GRAPH_REPRESENTATION
-@dataclass_json
-@dataclass
-class ExplicitSpan:
-    fileid: int
-    lo: int
-    hi: int
-
-
-@dataclass_json
-@dataclass
-class CondensedSpanGraph:
-    files: list[Path]
-    elts: list[ExplicitSpan]  # full spans for functions
-    nodes: list[list[int]]  # indices into `elts`
-    edges: list[tuple[int, int]]  # indices into `nodes`
-
-
-class SpeculativeFileRewriter:
-    def __init__(self, p: Path):
-        self.p = p
-        self.original_content = p.read_text("utf-8")
-        self.cached_content = self.original_content
-
-    def update_content_via(self, fn: Callable[[str], str]):
-        self.cached_content = fn(self.cached_content)
-
-    def write(self) -> bool:
-        """Returns true if any changes were made."""
-        if self.cached_content != self.original_content:
-            self.p.write_text(self.cached_content, encoding="utf-8")
-            return True
-        return False
-
-    def restore(self):
-        self.p.write_text(self.original_content, encoding="utf-8")
-
-
-class SpeculativeSpansEraser:
-    def __init__(
-        self, spans: Sequence[ExplicitSpan | None], span_to_path: Callable[[ExplicitSpan], Path]
-    ):
-        self.spans = spans
-        self.rewriters: dict[Path, SpeculativeFileRewriter] = {}
-        self.spans_by_path: dict[Path, list[ExplicitSpan]] = {}
-
-        for span in spans:
-            if span is not None:
-                file_path = span_to_path(span)
-                if file_path not in self.rewriters:
-                    self.spans_by_path.setdefault(file_path, []).append(span)
-                    self.rewriters[file_path] = SpeculativeFileRewriter(file_path)
-
-    def erase_spans(self):
-        def erase_span(span: ExplicitSpan, content: str) -> str:
-            """Erase the span from the content."""
-            assert span.hi > span.lo, "Span must be non-empty"
-            assert span.hi <= len(content), "Span end must be within content bounds"
-            return content[: span.lo] + (" " * (span.hi - span.lo)) + content[span.hi :]
-
-        for rewriter in self.rewriters.values():
-            for span in self.spans_by_path[rewriter.p]:
-                rewriter.update_content_via(lambda content, span=span: erase_span(span, content))
-            rewriter.write()
-
-    def restore(self):
-        for rewriter in self.rewriters.values():
-            rewriter.restore()
-
-
-class SpeculativeFilesEraser:
-    def __init__(self, files: Sequence[Path]):
-        self.files = files
-        self.rewriters: dict[Path, SpeculativeFileRewriter] = {
-            file: SpeculativeFileRewriter(file) for file in files
-        }
-
-    def add_file(self, file: Path) -> SpeculativeFileRewriter:
-        if file not in self.rewriters:
-            self.rewriters[file] = SpeculativeFileRewriter(file)
-
-        return self.rewriters[file]
-
-    def restore(self):
-        for rewriter in self.rewriters.values():
-            rewriter.restore()
 
 
 def run_un_unsafe_improvement(root: Path, dir: Path):
