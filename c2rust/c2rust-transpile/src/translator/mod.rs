1be078258a142c3452b3c0afc944a6a78d48f29a
Ben Karel <bkarel@aarno-labs.com>
2025-08-30 06:12:37 -0400

    NFC: Shift placement of mut annotation

--- c2rust/c2rust-transpile/src/translator/mod.rs
+++ c2rust/c2rust-transpile/src/translator/mod.rs
@@ -3034,7 +3034,7 @@ impl<'c> Translation<'c> {
                     (ty, init)
                 };
 
-                let static_def = if is_externally_visible {
+                let mut static_def = if is_externally_visible {
                     mk_linkage(false, new_name, ident).pub_().extern_("C")
                 } else if self.cur_file.borrow().is_some() {
                     mk().pub_()
@@ -3044,7 +3044,7 @@ impl<'c> Translation<'c> {
 
                 // Force mutability due to the potential for raw pointers occurring in the type
                 // and because we may be assigning to these variables in the external initializer
-                let mut static_def = static_def.span(span).mutbl();
+                static_def = static_def.span(span).mutbl();
                 if has_thread_duration {
                     static_def = static_def.single_attr("thread_local");
                 }
