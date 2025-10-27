4adc29c583f43477ae20bfc6181c02885ddb5b2e
brk <bkarel@aarno-labs.com>
2025-10-27 11:50:46 -0400

    Pull in `cc2json` which emits call sites as structured JSON (#130)
    Specifically https://github.com/brk/cclyzerpp/commit/751a0ea9764eb98a72786f2f0382377b2b3d881b
    via https://github.com/Aarno-Labs/tenjin-build-deps/releases/tag/rev-c7875c5b5

--- cli/constants.py
+++ cli/constants.py
@@ -10,7 +10,7 @@ WANT = {
     "10j-build-deps": "rev-b578a0937",
     # Note that 10j-more-deps builds against a specific version LLVM, so before
     # upgrading the major version of 10j-llvm, update 10j-more-deps first.
-    "10j-more-deps": "rev-3b85e47e7",
+    "10j-more-deps": "rev-c7875c5b5",
     "10j-codehawk": "d7904d71d7743369fab9020fcaf3e8b15482152b",
     "10j-codehawk-c": "92b9bbd0220281e2931bd66b83c0f6c8ab911cde",
     # Keep in sync with the version in `xj-improve-multitool/rust-toolchain.toml`.
