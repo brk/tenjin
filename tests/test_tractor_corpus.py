33b6e3def8b0a86331c2dc28671ab1e2d0bbb679
Ben Karel <bkarel@aarno-labs.com>
2026-09-16 19:46:00 -0400

    Avoid cross-TU replication of local function pointer types

--- tests/test_tractor_corpus.py
+++ tests/test_tractor_corpus.py
@@ -1804,7 +1804,7 @@ def test_tractor_b2_organic_underhanded_c_luggage_app(tenjin_fixtures: TenjinFix
 def test_tractor_b2_synthetic_tu_linkage_app(tenjin_fixtures: TenjinFixtures):
     case_dir = "Public-Tests/B02_synthetic/tu_linkage"
     eval_tractor_ta3_corpus_app(tenjin_fixtures, case_dir)
-    assert get_final_unsafe_fns_count(tenjin_fixtures.tmp_resultsdir) == 31
+    assert get_final_unsafe_fns_count(tenjin_fixtures.tmp_resultsdir) == 29
 
 
 # ██████╗  █████╗ ████████╗████████╗███████╗██████╗ ██╗   ██╗    ██████╗     ██╗     ██╗██████╗ ███████╗
